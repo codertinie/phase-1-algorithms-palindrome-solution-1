213efa67ace167d8a76e5d864b31799402e8affb
codertinie <martin.munyao@moringaschool.com>
2022-09-12 20:08:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+function reverseString(word) {
+  // TODO: implement string reversing functionality
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  if the reversed string is same as the input
+  return true else return false
 */
 
 /*
   Add written explanation of your solution here
+  the function is palindrome takes one argument and checks whether it's a
+  palindrome. it does so by reversing the input then compares the reversed input
+  to the original input. If they are equal, it returns true and returns false if otherwise.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +34,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
 }
 
 module.exports = isPalindrome;
